7677d9e96897f94fdbc7a9e54753a8476d51c95f
Dw4lt <142900824+Dw4lt@users.noreply.github.com>
2023-11-20 00:33:47 +0100

    Add more feedback to CLI

--- src/yawn/__main__.py
+++ src/yawn/__main__.py
@@ -1,3 +1,6 @@
+import os
+os.environ.setdefault("TQDM_DISABLE", "1") # HACK: Downloading a model has its own progress bar, loading it locally does not. There is no way to tell them apart ¯\_(ツ)_/¯
+
 from .main import main
 
 main()
--- src/yawn/main.py
+++ src/yawn/main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import threading
 import time
 
@@ -7,6 +8,7 @@ import numpy as np
 import pyaudio
 import torch
 import whisper
+from halo import Halo
 
 CHUNKSIZE = 1024
 SAMPLING_RATE = 16000
@@ -17,6 +19,7 @@ class Recorder:
         self.recording_thread: threading.Thread = None
         self.stop_requested = threading.Event()
         self.frames: list[np.ndarray] = []
+        self.recording_spinner = Halo(text="Recording", spinner='dots')
 
     def _record(self):
         p = pyaudio.PyAudio()
@@ -34,19 +37,20 @@ class Recorder:
 
     def start_recording(self):
         if self.recording_thread is None:
-            print("recording on")
+            self.recording_spinner.start()
             self.recording_thread = threading.Thread(target=self._record)
             self.recording_thread.start()
 
     def stop_recording(self):
-        print("stopping recording")
         self.stop_requested.set()
         if self.recording_thread and self.recording_thread.is_alive():
             self.recording_thread.join()
             self.stop_requested.clear()
         self.recording_thread = None
+        self.recording_spinner.stop()
 
 
+@Halo("Processing audio", spinner="dots")
 def pre_process_audio(frames):
     audio_data = np.concatenate(frames) # Consolidate list of arrays
     audio_data = np.frombuffer(audio_data, np.int16).flatten().astype(np.float32) / 32768.0 # type conversion magic
@@ -54,7 +58,8 @@ def pre_process_audio(frames):
 
 
 def transcribe(model: whisper.Whisper, audio: np.ndarray) -> str:
-    print("transcribing")
+    progress = Halo("Transcribing", spinner="dots").start()
+
     # pad/trim audio to match expected model input
     audio = whisper.pad_or_trim(audio)
 
@@ -63,6 +68,8 @@ def transcribe(model: whisper.Whisper, audio: np.ndarray) -> str:
 
     # decode
     res = model.transcribe(audio, fp16=False, language="en")
+
+    progress.stop()
 
     text = res["text"].strip()
     lang = res["language"]
@@ -110,18 +117,25 @@ def stop_recording(model: whisper.Whisper, recorder: Recorder):
 
 
 def main():
+    status = Halo(spinner="dots")
     args = handle_command_arguments()
 
-    print(f"Cuda: {torch.cuda.is_available()}")
+    if torch.cuda.is_available():
+        status.succeed("Cuda enabled.")
+    else:
+        status.fail("Cuda not availabe, using CPU instead.")
 
     recorder = Recorder()
-    model = whisper.load_model(args.model)
 
+    status.start("Loading model")
+    model = whisper.load_model(args.model)
+    status.succeed("Model loaded.")
 
     keyboard.add_hotkey(args.hotkey, record, args=[recorder], suppress=True, trigger_on_release=False)
     keyboard.add_hotkey(args.hotkey, stop_recording, args=[model, recorder], suppress=True, trigger_on_release=True)
 
-    print("ready!")
+    status.succeed("Ready!")
+    status.info(f"Press and hold `{args.hotkey}` to start recording.")
+
     while True:
         time.sleep(10)
-
